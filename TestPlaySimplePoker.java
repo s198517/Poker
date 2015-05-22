f54c5d05419bbffb0c000745fcaf71d722b7ff62
Simen Arvnes <simenarvnes@gmail.com>
2015-05-22 12:27:18 -0700

    Update TestPlaySimplePoker.java

--- TestPlaySimplePoker.java
+++ TestPlaySimplePoker.java
@@ -1,16 +1,3 @@
-/*************************************************************************************
- *
- *  This program is used to test PJ5.SimplePoker class
- *  More info are given in Readme file
- * 
- *  PJ5 class allows user to run program as follows:
- *
- *    	java PJ5		    // default credit is $100
- *  or 	java PJ5 NNN		// set initial credit to NNN
- *
- *  Do not modify this file!
- *
- **************************************************************************************/
 
 import PJ5.SimplePoker;
 
